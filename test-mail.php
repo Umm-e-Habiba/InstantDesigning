b287ff2bbc3f948e73d6e60c81de6edfe9b3448e
umm-e-habiba <ummehabiba989@gmail.com>
2024-11-15 12:00:29 -0800

    version 2

--- test-mail.php
+++ test-mail.php
@@ -13,13 +13,13 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'ummehabiba989@gmail.com';
-    $mail->Password = 'ujim fgaf dwej lndl';
+    $mail->Password = '***************';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
     // Recipients
-    $mail->setFrom('ummehabiba989@gmail.com', 'Umm-e-habiba');
-    $mail->addAddress('ummehabiba989@gmail.com');
+    $mail->setFrom('a989@gmail.com', 'Umm-e-habiba');
+    $mail->addAddress('a989@gmail.com');
 
     // Content
     $mail->isHTML(true);
